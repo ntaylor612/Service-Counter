6e9652e5c45cc88e10a773ee41291a42590ee700
ntaylor612 <nataliegracetaylor612@gmail.com>
2025-03-12 09:30:25 -0400

    Create outline_for_code.py

---
+++ outline_for_code.py
@@ -0,0 +1,28 @@
+# start; welcome descriptions
+
+# get name of owner
+# get make and model of the vehicle
+# get the current mileage of the vehicle
+
+# ask if oil change + tire rotation
+#   if yes
+#       give options (include a nevermind go to next button)
+#   if no go to next
+
+# ask if Brake Pads and Front End Alignment
+#   if yes
+#       give option (include a nevermind go to next button)
+#   if no go to next
+
+# ask if Broken Glass Repair
+#   if yes
+#       give options (include a nevermind go to next button)
+#   if no go to next
+
+# ask if dent removal
+#   if yes
+#       give options (include a nevermind go to next button)
+#   if no go to end
+
+# write work order with all the services chosen
+# write invoice with: owner and car info
